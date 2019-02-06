d3f190d9a446110433dea3a563be3b73915cf469
EunHyunCho <ouoyy6395@gamil.com>
2019-02-06 13:41:10 +0900

    Initial commit

---
+++ src/main/java/my/examples/blogExam/BlogExamApplication.java
@@ -0,0 +1,14 @@
+package my.examples.blogExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogExamApplication.class, args);
+	}
+
+}
+
